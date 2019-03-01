b2b838123e165e3e39ce1150cbb1965d1b78455a
Unknown <alikarakaya92@gmail.com>
2019-03-01 13:49:27 +0300

    Inflow matlab model is created.

--- CreateInflowMatrices.m
+++ CreateInflowMatrices.m
@@ -1,4 +1,4 @@
-function[cosStates,Mcos,Lcos,GamaCos,sinStates,Msin,Lsin,GamaSin] = CreateInflowMatrices(r,k)
+function[stateCount,Mcos,invLc,Msin,invLs] = CreateInflowMatrices(r,k,SkewAngleRad)
 % Enter R and J to CreateMatrixNumeric function
 % It will create the [superscript,subscript] values for each states in
 % cosStates and sinStates cell array.
@@ -7,22 +7,22 @@ function[cosStates,Mcos,Lcos,GamaCos,sinStates,Msin,Lsin,GamaSin] = CreateInflow
 %Get State Numbers
 countCosStates = length(cosStates);
 countSinStates = length(sinStates);
-
+stateCount = countCosStates + countSinStates;
 %Initialize Mass matricie
 Mcos    = zeros(countCosStates);
-Lcos    = sym(zeros(countCosStates));
+Lcos    = zeros(countCosStates);
 GamaCos = zeros(countCosStates);
 Msin    = zeros(countSinStates);
-Lsin    = sym(zeros(countSinStates));
+Lsin    = zeros(countSinStates);
 GamaSin = zeros(countSinStates);
-syms X;
+
 for ii = 1:1:countCosStates
 % State Loop    
     for jj = 1:1:countCosStates
     % Create interstate values
-    Mcos(ii,jj) = MFunc(cosStates{ii}(1),cosStates{ii}(2),cosStates{jj}(1),cosStates{jj}(2));
-    Lcos(ii,jj) = GainCSkewFunc(cosStates{ii}(1),cosStates{ii}(2),cosStates{jj}(1),cosStates{jj}(2),X);
-    GamaCos(ii,jj) = GamaFunc(cosStates{ii}(1),cosStates{ii}(2),cosStates{jj}(1),cosStates{jj}(2));
+    Mcos(ii,jj) = MFunc(cosStates(ii,1),cosStates(ii,2),cosStates(jj,1),cosStates(jj,2));
+    Lcos(ii,jj) = GainCSkewFunc(cosStates(ii,1),cosStates(ii,2),cosStates(jj,1),cosStates(jj,2),SkewAngleRad);
+    GamaCos(ii,jj) = GamaFunc(cosStates(ii,1),cosStates(ii,2),cosStates(jj,1),cosStates(jj,2));
     end
     
 end
@@ -31,12 +31,15 @@ for ii = 1:1:countSinStates
 % State Loop    
     for jj = 1:1:countSinStates
     % Create interstate values
-    Msin(ii,jj) = MFunc(sinStates{ii}(1),sinStates{ii}(2),sinStates{jj}(1),sinStates{jj}(2));
-    Lsin(ii,jj) = GainSSkewFunc(sinStates{ii}(1),sinStates{ii}(2),sinStates{jj}(1),sinStates{jj}(2),X);
-    GamaSin(ii,jj) = GamaFunc(sinStates{ii}(1),sinStates{ii}(2),sinStates{jj}(1),sinStates{jj}(2));
+    Msin(ii,jj) = MFunc(sinStates(ii,1),sinStates(ii,2),sinStates(jj,1),sinStates(jj,2));
+    Lsin(ii,jj) = GainSSkewFunc(sinStates(ii,1),sinStates(ii,2),sinStates(jj,1),sinStates(jj,2),SkewAngleRad);
+    GamaSin(ii,jj) = GamaFunc(sinStates(ii,1),sinStates(ii,2),sinStates(jj,1),sinStates(jj,2));
     end
     
 end
+
+invLc = inv(Lcos .* GamaCos);
+invLs = inv(Lsin .* GamaSin);
 
 % for ii = 1:1:length(c)
 %     disp(c{ii});
